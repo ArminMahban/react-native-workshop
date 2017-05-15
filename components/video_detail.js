685247de1953c53823ef810b8f06f5a5391445f4
Jane Lee <jane.lee.19@dartmouth.edu>
2017-05-15 11:24:37 -0400

    pushing youtube video detail

--- components/video_detail.js
+++ components/video_detail.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
+import YouTube from 'react-native-youtube'
 
 import {
     StyleSheet,
@@ -29,13 +30,26 @@ var styles = StyleSheet.create({
 class VideoDetail extends Component {
     render() {
         var book = this.props.book;
-        var imageURI = (typeof book.volumeInfo.imageLinks !== 'undefined') ? book.volumeInfo.imageLinks.thumbnail : '';
         var description = (typeof book.volumeInfo.description !== 'undefined') ? book.volumeInfo.description : '';
         return (
-            <View style={styles.container}>
-                <Image style={styles.image} source={{uri: imageURI}} />
-                <Text style={styles.description}>{description}</Text>
-            </View>
+          <View style={styles.container}>
+            <YouTube
+              ref="youtubePlayer"
+              videoId="KVZ-P-ZI6W4" // The YouTube video ID
+              play={true}           // control playback of video with true/false
+              hidden={false}        // control visibility of the entire view
+              fullscreen={true}    // control whether the video should play inline
+              loop={false}          // control whether the video should loop when ended
+
+              onReady={(e)=>{this.setState({isReady: true})}}
+              onChangeState={(e)=>{this.setState({status: e.state})}}
+              onChangeQuality={(e)=>{this.setState({quality: e.quality})}}
+              onError={(e)=>{this.setState({error: e.error})}}
+              onProgress={(e)=>{this.setState({currentTime: e.currentTime, duration: e.duration})}}
+
+              style={{alignSelf: 'stretch', height: 300, backgroundColor: 'black', marginVertical: 10}}
+            />
+          </View>
         );
     }
 }
